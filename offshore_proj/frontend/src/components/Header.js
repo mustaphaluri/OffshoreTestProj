ff3a5ad04b3bd8984084675a5c7ef8cc5b27577d
mustaphaluri <71587782+mustaphaluri@users.noreply.github.com>
2020-11-30 16:43:26 +0000

    frontend added
    created a frontend and added basic components. ie header

---
+++ offshore_proj/frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+const Header = () => (
+  <div>
+    <Header as="h1">Offshore Test App Built By Mustapha From Ghana </Header>
+   
+  </div>
+);
+
+export default Header;
